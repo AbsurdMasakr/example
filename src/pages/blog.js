38acc411f91a397b145e214762a3339b35a58b06
Jakub Komorowski <absurdmasakra@gmail.com>
2019-11-28 00:02:33 +0100

    first steps in tours

---
+++ src/pages/blog.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Blog = () => {
+  return <div>hello from blog</div>
+}
+
+export default Blog
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => {
+  return (
+    <div>
+      Hello World<Link to="/blog">blog</Link>
+    </div>
+  )
+}
+
+export default IndexPage

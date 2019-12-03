aa8dce4e058c0ce6e1fc22fa61f1e9595d7de3b2
Jakub Komorowski <absurdmasakra@gmail.com>
2019-12-03 02:41:36 +0100

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function contact() {
-  return <div>hello form contact</div>
+  return <Layout>hello form contact</Layout>
 }
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"
+import Layout from "../components/Layout"
 
 export default class tours extends Component {
   render() {
-    return <div>hello from the tour page!</div>
+    return <Layout>hello from the tour page!</Layout>
   }
 }
